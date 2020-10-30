bb16c2fd0500da02788090753dfe7bc7aa65004d
divyesh1612 <divyesh1612@users.noreply.github.com>
2020-10-30 17:35:12 -0400

    Updated User model and DataAccess files

---
+++ AdminLogIn/DataAccess.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace AdminLogIn
+{
+    public class DataAccess
+    {
+        public DataAccess()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["myConnection"].ConnectionString;
+
+        }
+        private string _connectionString;
+
+        public string ConnectionString => _connectionString;
+
+        SqlConnection con = new SqlConnection(ConnectionString);
+        public int AddUser(DataAccess ObjBO)
+        {
+            try
+            {
+                string query = "insert into Users (Email, Password) values ('" + ObjBO.Email + "','" + ObjBO.Password + "');";
+                SqlCommand cmd = new SqlCommand(query, con);
+                con.Open();
+                int Result = cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                return Result;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
--- AdminLogIn/Models/Users.cs
+++ AdminLogIn/Models/Users.cs
@@ -1,24 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
 namespace AdminLogIn.Models
 {
-    public class User : DataObject
+    public class User : DataAccess
     {
         private string email;
         private string password;
 
+        [Required(ErrorMessage = "Email is required")]
+        [Display(Name = "Please enter Email")]
         public string Email
         {
             get { return email; }
             set { email = value; }
         }
+
+        [Required(ErrorMessage = "Password is required")]
+        [Display(Name = "Please enter Password")]
         public string Password
         {
             get { return password; }
             set { password = value; }
         }
+
+        
     }
+
+   
+
 }
