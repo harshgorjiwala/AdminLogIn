fe9ff819154e55655ec1bd01ed8c26d29cc732e9
divyesh1612 <divyesh1612@users.noreply.github.com>
2020-10-30 13:55:20 -0400

    Created Users-model and connectionstring

---
+++ AdminLogIn/DataObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace AdminLogIn
+{
+    public class DataObject
+    {
+        public DataObject()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["myConnection"].ConnectionString;
+
+        }
+        private string _connectionString;
+
+        public string ConnectionString => _connectionString;
+    }
+}
---
+++ AdminLogIn/Models/Users.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdminLogIn.Models
+{
+    public class User : DataObject
+    {
+        private string email;
+        private string password;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+}
